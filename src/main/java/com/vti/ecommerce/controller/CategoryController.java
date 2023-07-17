94c7b5ad6784f5d938b83c783bba34be104a5e36
viet.nguyenquoc2 <viet.nguyenquoc2@vti.com.vn>
2023-07-17 16:12:11 +0700

    update

--- src/main/java/com/vti/ecommerce/controller/CategoryController.java
+++ src/main/java/com/vti/ecommerce/controller/CategoryController.java
@@ -2,10 +2,8 @@ package com.vti.ecommerce.controller;
 
 
 import com.vti.ecommerce.dto.CategoryDTO;
-import com.vti.ecommerce.model.Category;
 import com.vti.ecommerce.response.ResponseData;
 import com.vti.ecommerce.service.CategoryService;
-import jakarta.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin/category")
